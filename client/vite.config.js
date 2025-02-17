e3b363080be1bf9f81e449160a3e1344267a372d
Shikher Patel <shikherpatel@gmail.com>
2025-02-17 22:52:39 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
+    outDir: 'dist', // Ensure Vite outputs to `dist`
+    chunkSizeWarningLimit: 1000, // Suppress large chunk warnings
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000", // Allow local API proxy for dev mode
+    },
   },
 });

d579e782fe88721f79a5437776f5277da1596b62
Shikher Patel <shikherpatel@gmail.com>
2025-02-17 17:05:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,15 +8,14 @@ import budgetRoutes from "./routes/budget.js";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/budget", budgetRoutes);
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-app.listen(PORT, () => console.log("Server running on port 5000"));
+export default app;  // Export app for serverless
